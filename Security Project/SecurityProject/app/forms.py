c004028d7b424419a22ad0cf59dd0d85ed77d8a5
LayanSalman <146759388+LayanSalman@users.noreply.github.com>
2025-05-09 23:39:29 +0300

    Update forms.py

--- Security Project/SecurityProject/app/forms.py
+++ Security Project/SecurityProject/app/forms.py
@@ -5,12 +5,20 @@ from wtforms.validators import DataRequired, Length
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
+    #Secure
+    # password = PasswordField('Password', validators=[DataRequired()])
+
+    #InSecure
+    password = StringField('Password')  # Removed DataRequired
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    #Secure
+    #password = PasswordField('Password', validators=[DataRequired()])
+    
+    #InSecure
+    password = StringField('Password')  # Removed DataRequired
     submit = SubmitField('Login')
 
 class CommentForm(FlaskForm):
